4de0a26759b1145fadf8b9d149177cca57f146d9
Zachary J. Myers <zmyers550@gmail.com>
2018-05-13 23:22:15 -0400

    Delete serverroles.js

--- commands/commands/serverroles.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-    const role = message.guild.roles;
-  const embed = new Discord.RichEmbed()
-    .addField("Server Roles", role.map((e) => e).join(', '))
-  message.channel.send({embed}) 
-}
-   
