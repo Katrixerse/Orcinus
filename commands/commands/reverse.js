38227e3581cdf47dde996ef43a2040c6aa12c603
Zachary J. Myers <zmyers550@gmail.com>
2018-05-13 23:17:23 -0400

    Delete reverse.js

--- commands/commands/reverse.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-        const text = args.join(' ')
-        const converted = text.split('').reverse().join('');
-        message.channel.send(`\u180E${converted}`);
-}
-   
