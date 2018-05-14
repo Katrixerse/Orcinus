4da44ce7c97e560cb4e2b385f6b696e92c290fe9
Zachary J. Myers <zmyers550@gmail.com>
2018-05-13 23:22:41 -0400

    Delete sexyrate.js

--- commands/commands/sexyrate.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-    const sexyrate = Math.floor(Math.random() * 100)
-       const embed = new Discord.RichEmbed()
-            .addField(":heart_decoration: Sexy Rate :heart_decoration: ", "I rate you a " + sexyrate + " out of 100 on the sexy scale")
-            .setThumbnail(message.author.displayAvatarURL)
-       message.channel.send({embed})
-}
