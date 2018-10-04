aa17827e94b47c0fd2b42a501bbe8ff52e90db9f
Zachary J. Myers <zmyers550@gmail.com>
2018-10-04 11:19:41 -0400

    Update softban.js

--- commands/softban.js
+++ commands/softban.js
@@ -19,7 +19,7 @@ exports.run = (client, message, args) => {
     if (message.mentions.users.size < 1) return message.channel.send(usage)
     let user = message.guild.member(message.mentions.users.first());
     if (user.highestRole.position >= message.member.highestRole.position) return message.reply('I cant softban that member. They are the same level as you or higher. :x:');
-    let modlog = message.guild.channels.find('name', row.logschannel);
+    let modlog = message.guild.channels.find(channel => channel.name == row.logschannel);
     if (!message.guild.member(user).bannable) return message.reply('This member is not banable. Perhaps they have a higher role than me?');
     if (reason.length < 1) return;
     message.channel.send("***The User has been successfully been soft banned! :white_check_mark:***")
