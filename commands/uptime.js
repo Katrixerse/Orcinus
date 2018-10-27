f898f1788a8c3861158c08c5abce16dde92258d2
Zachary J. Myers <zmyers550@gmail.com>
2018-10-27 16:29:37 -0400

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const moment = require('moment');
+require('moment-duration-format');
 exports.run = (client, message, args) => {
+  try {
     const embed = new Discord.RichEmbed()
       .setTitle("Bot Uptime")
-      .addField("Day(s)", (Math.round(client.uptime / (1000 * 60 * 60 * 24))))
-      .addField("Hour(s)", (Math.round(client.uptime / (1000 * 60 * 60)) % 24))
-      .addField("Minute(s)", (Math.round(client.uptime / (1000 * 60)) % 60))
-      .addField("Second(s)", (Math.round(client.uptime / 1000) % 60))
+      .addField('Uptime:', moment.duration(client.uptime).format('d [days], h [hours], m [minutes], s [seconds]', { trim: "small" }), true)
       .setColor(0x00A2E8)
-     message.channel.send({embed})
+     message.channel.send(embed)
+  } catch (err) {
+      message.channel.send(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+  }
 }
