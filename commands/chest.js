b23da7dc3466f529f68f594b26ea459384c010ae
Zachary J. Myers <zmyers550@gmail.com>
2019-03-02 12:09:31 -0500

    Delete chest.js

--- commands/chest.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const sql = require("sqlite");
-sql.open("./assets/guildsettings.sqlite");
-exports.run = (client, message, args) => {
-    
-}
